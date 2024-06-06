36e1132db116d8b5cc5c507e792c4da649f150f5
codenisi <bartholanisi@gmail.com>
2024-06-06 08:08:45 +0530

    Add files via upload

---
+++ app/Feedback.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+use App\Feedback;
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    protected $table='feedback';
+
+    protected $fillable = [
+        'request_id' ,'description',
+    ];
+
+}
+?>
---
+++ app/Http/Controllers/custom/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\custom;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    
+     
+}
+
+?>
